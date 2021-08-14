232e068003610d9ae23cafca1c63207eef2cbf3a
joaokage1 <j_vgomes@hotmail.com>
2021-08-14 16:23:57 -0300

    Basic authentication - DISABLING CSRF

--- src/main/java/com/security/amigos/code/security/ApplicationSecurityConfig.java
+++ src/main/java/com/security/amigos/code/security/ApplicationSecurityConfig.java
@@ -25,7 +25,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/v1/students/*").hasRole(STUDENT.name())
                 .anyRequest()
