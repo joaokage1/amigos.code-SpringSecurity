bd4e29a7153f50952c4b5778601428f66ddf543c
joaokage1 <j_vgomes@hotmail.com>
2021-08-24 10:04:35 -0300

    Form authentication - CUSTOM LOGIN PAGE

--- src/main/java/com/security/amigos/code/security/ApplicationSecurityConfig.java
+++ src/main/java/com/security/amigos/code/security/ApplicationSecurityConfig.java
@@ -36,7 +36,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login").permitAll();
     }
 
     @Override
