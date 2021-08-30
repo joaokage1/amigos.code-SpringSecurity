88ed4b188d22701534a37494f097471bfa4ccb99
Joao Vitor Gomes <jvgsilva180@gmail.com>
2021-08-30 14:31:23 -0300

    Form authentication - DB AUTHENTICATION OVERVIEW

---
+++ src/main/java/com/security/amigos/code/auth/ApplicationUser.java
@@ -0,0 +1,23 @@
+package com.security.amigos.code.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ApplicationUser implements UserDetails {
+
+    private final List<? extends GrantedAuthority> authorities;
+    private final String username;
+    private final String password;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+
+}
